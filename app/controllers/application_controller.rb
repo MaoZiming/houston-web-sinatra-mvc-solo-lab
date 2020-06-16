50d3a78e46e6d18f737225cd772e20b6f3224b89
ZIming Mao <ziming.mao@yale.edu>
2020-06-16 15:08:39 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,47 @@
 class ApplicationController < Sinatra::Base
-    configure do
-        set :views, 'app/views'
-        set :method_override, true
-      end
     
-      get '/' do
-        erb :welcome
-      end
+    configure do
+      set :views, 'app/views'
+      set :method_override, true
+    end
+    
+    get '/' do
+      erb :welcome
+    end
 
-   
+    get '/puppies' do 
+      @puppies = Puppy.all
+      erb :index
+    end
+    
+    post '/puppies' do
+      puppy = Puppy.create(params)
+      redirect "/puppies/#{puppy.id}"
+    end
+
+    get '/puppies/new' do
+      erb :new
+    end
+
+    get '/puppies/:id/edit' do
+      @puppy = Puppy.find(params[:id])
+      erb :edit
+    end
+
+    get '/puppies/:id' do
+      @puppy = Puppy.find(params[:id])
+      erb :show
+    end
+
+    patch '/puppies/:id' do
+      puppy = Puppy.find(params[:id])
+      puppy.update(name: params[:name], breed: params[:breed])
+      redirect "/puppies/#{puppy.id}"
+    end
+    delete '/puppies/:id' do
+      puppy = Puppy.find(params[:id])
+      puppy.destroy
+      redirect '/puppies'
+    end
+       
 end
--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,11 @@
 class PuppiesController < ApplicationController
    
+
+    set :views, "app/views"
+    set :method_override, true
+
+    get '/index' do
+
+        erb :index
+    end
 end
